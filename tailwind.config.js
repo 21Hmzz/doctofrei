c31c099a22a1d21e666204c7d79b5e4789c5389a
Elvis Cabral <ecabral@dotscreen.com>
2024-11-29 14:20:25 +0100

    WIP

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,ts}",], theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+          sans: ['Roboto', 'sans-serif'],
+      },
+  },
   }, plugins: [],
 }
 
