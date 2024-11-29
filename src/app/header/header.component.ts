86f2fedf88e975fb0a2a3341c2c4a636dce70dc0
Hamza Bella <hamza@stevegates.co>
2024-11-29 12:55:20 +0100

    fix isAuth

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {AuthService} from "../auth/auth.service";
 
 @Component({
@@ -6,14 +6,17 @@ import {AuthService} from "../auth/auth.service";
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
 
+
+  public isAuth: boolean = false;
 
   constructor(private authService: AuthService) {
   }
 
-  get isLoggedIn(): boolean {
-    return this.authService.isAuthenticated();
+  ngOnInit() {
+    this.isAuth = this.authService.isAuthenticated();
   }
+
 
 }
